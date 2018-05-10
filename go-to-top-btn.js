ace9a2532b995ef6484bb2a71a63899183edd170
Joanna Szylo <joannaszylo@gmail.com>
2018-05-10 16:37:03 +0100

    delete comments

--- go-to-top-btn.js
+++ go-to-top-btn.js
@@ -15,19 +15,3 @@ function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
-
-// window.onscroll = function() {scrollFunction();};
-
-// function scrollFunction() {
-//     if (document.body.scrollTop > 655 || document.documentElement.scrollTop > 655) {
-//         document.getElementById("page-Btn").style.display = "block";
-//     } else {
-//         document.getElementById("page-Btn").style.display = "none";
-//     }
-// }
-
-// // When the user clicks on the button, scroll to the top of the document
-// function topFunction() {
-//     document.body.scrollTop = 0;
-//     document.documentElement.scrollTop = 0;
-// }
