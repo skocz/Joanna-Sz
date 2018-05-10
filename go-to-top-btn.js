869897a58e45d360f6ca6452bec275460defb23e
Joanna Szylo <joannaszylo@gmail.com>
2018-05-10 18:35:52 +0100

    added only a bottom button to go up

--- go-to-top-btn.js
+++ go-to-top-btn.js
@@ -1,15 +1,14 @@
-// When the user scrolls down 20px from the top of the document, 
-//show the button
-window.onscroll = function() {scrollFunction();};
+// // When the user scrolls down 20px from the top of the document, 
+// //show the button
+// window.onscroll = function() {scrollFunction();};
 
-function scrollFunction() {
-    if ((document.body.scrollTop > 655 && screen.width < 500)|| (document.documentElement.scrollTop > 655 && screen.width < 500)){
-        document.getElementById("mobileBtn").style.display = "block";
-    } else {
-        document.getElementById("mobileBtn").style.display = "none";
-    }
-}
-
+// function scrollFunction() {
+//     if ((document.body.scrollTop > 655 && screen.width < 500)|| (document.documentElement.scrollTop > 655 && screen.width < 500)){
+//         document.getElementById("mobileBtn").style.display = "block";
+//     } else {
+//         document.getElementById("mobileBtn").style.display = "none";
+//     }
+// }
 
 
 // When the user clicks on the button, scroll to the top of the document
