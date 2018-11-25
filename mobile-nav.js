897288bb27662159a3ccff1b0e74d3cc48ef5b27
Joanna Szylo <joannaszylo@gmail.com>
2018-11-25 21:54:41 +0000

    added skills chart bar- new section

--- mobile-nav.js
+++ mobile-nav.js
@@ -1,31 +1,31 @@
-      /*
-       * Open the drawer when the menu ison is clicked.
-       */
-      window.onload=function(){
-      let menu = document.querySelector('#menu');
-      let main = document.querySelector('main');
-      let drawer = document.querySelector('.core-nav');
+/*
+ * Open the drawer when the menu ison is clicked.
+ */
+window.onload=function(){
+  let menu = document.querySelector('#menu');
+  let main = document.querySelector('main');
+  let drawer = document.querySelector('.core-nav');
 
-      menu.addEventListener('click', function(e) {
-        drawer.classList.toggle('open');
-        e.stopPropagation();
-        console.log(e);
-      });
-      main.addEventListener('click', function() {
-        drawer.classList.remove('open');
-        console.log(e);
-      });
+  menu.addEventListener('click', function(e) {
+    drawer.classList.toggle('open');
+    e.stopPropagation();
+    console.log(e);
+  });
+  main.addEventListener('click', function() {
+    drawer.classList.remove('open');
+    console.log(e);
+  });
     
 
-window.onscroll = function() {myFunction()};
+  window.onscroll = function() {myFunction()};
 
-//var navbar = document.getElementById("navbar");
-var sticky = drawer.offsetTop;
+  //var navbar = document.getElementById("navbar");
+  var sticky = drawer.offsetTop;
 
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-  } else {
-    drawer.classList.remove("open");
+  function myFunction() {
+    if (window.pageYOffset >= sticky ) {
+    } else {
+      drawer.classList.remove("open");
+    }
   }
-}
 }
